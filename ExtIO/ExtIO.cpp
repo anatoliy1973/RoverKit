1259fb2e08c88977bde9ce232b124274314734a7
anatoliy1973 <anatoliy_lokshin@mail.ru>
2015-05-04 15:32:53 +0300

    Added ExtIO project

---
+++ ExtIO/ExtIO.cpp
@@ -0,0 +1,16 @@
+/*
+ * ExtIO.cpp
+ *
+ * Created: 5/4/2015 15:28:02
+ *  Author: Tolik
+ */ 
+ /*
+#include <avr/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code
+
+    return 0;
+}
+*/
